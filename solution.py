6e954afe240179b5867ab253dd9d86ba7b544974
d1scob4ll <130067358+d1scob4ll@users.noreply.github.com>
2023-04-06 19:48:42 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import numpy as np
 chat_id = 305011093 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean()/(4**2) # Ваш ответ
+    n = len(x)
+    s = np.sum(x)
+    estimate = 2 * s / (n * 4**2)
+    return estimate
